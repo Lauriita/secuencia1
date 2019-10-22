4eb4288be61bfd6873aab9228931373b01de0887
Lauriita <laurahb123@gmail.com>
2019-10-22 12:30:07 +0200

    Agregar archivos de proyecto.

---
+++ Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+
+    Dim inter As Integer
+    Dim salida As String
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        inter = TextBox2.Text
+
+        For n3 As Integer = 0 To inter Step 2
+
+            salida = n3
+            TextBox1.Text = TextBox1.Text + salida & vbNewLine
+
+
+        Next
+
+    End Sub
+End Class
